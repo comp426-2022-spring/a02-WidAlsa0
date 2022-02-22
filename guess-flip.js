c9d7a44d04b4b4f3c917933404700b8faa9b4b10
WidAlsa0 <69405330+WidAlsa0@users.noreply.github.com>
2022-02-22 12:16:49 -0500

    slice instead of split

--- guess-flip.js
+++ guess-flip.js
@@ -7,7 +7,7 @@ if (arg[0] == null) {
     console.error(error)
     console.log("Usage: node guess-flip --call=[heads|tails]")
 } else {
-    call = arg[0].split("--call=")[1]
+    call = arg[0].slice[7]
     if (call == null || (call != "heads" || "tails")) {
         console.log("Usage: node guess-flip --call=[heads|tails]")
     } else {
