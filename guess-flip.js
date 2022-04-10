6615971e67fb8de90e7102840d710e7a74da9e4b
WidAlsa0 <69405330+WidAlsa0@users.noreply.github.com>
2022-04-10 01:58:53 -0400

    a02 update

--- guess-flip.js
+++ guess-flip.js
@@ -4,7 +4,7 @@ import minimist from 'minimist'
 
 const arg = minimist(process.argv.slice(2))
 
-if (arg == 'heads' || arg == 'tails') {
+if (arg.call == 'heads' || arg.call == 'tails') {
     console.log(flipACoin(arg.call))
 } else {
     console.log("Error: no input.")
